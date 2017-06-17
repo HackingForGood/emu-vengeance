17bdea2ab2530276918767586b13c91c1f4baaa9
Nick <njgeor16@gmail.com>
2017-06-17 12:46:17 -0400

    Unneccessary UI stuff

--- app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
+++ app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +17,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    public void onSignUpButtonClick(){
+        Intent toSignUp = new Intent(this, SignUp.class);
+        startActivity(toSignUp);
     }
 
     @Override
