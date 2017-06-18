383417362393162ee0ce2c074185889c1c1215af
Will <willzhang100@gmail.com>
2017-06-18 11:17:09 -0400

    Implemented basic Firebase authentication using FirebaseUI Auth.

--- app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
+++ app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
@@ -1,49 +1,109 @@
 package com.vengeance.emu.hackingforgood;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.firebase.analytics.FirebaseAnalytics;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.ResultCodes;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
+import static android.R.id.message;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "EMU";
-    private FirebaseAnalytics mFirebaseAnalytics;
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "MainActivity";
+    private static final int RC_SIGN_IN = 100;
+
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        if(mFirebaseAuth.getCurrentUser() != null) {
+            // Already signed in
+            startActivity(new Intent(this, Give.class));
+        } else {
+            // Not signed in
+            Log.d(TAG, "Not signed in.");
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                            .setAvailableProviders(
+                                    Arrays.asList(
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                    .build(),
+                    RC_SIGN_IN
+            );
+        }
+    }
 
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == ResultCodes.OK) {
+                IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+                String type = idpResponse.getProviderType();
+                Log.d(TAG, type);
+                Snackbar.make(findViewById(R.id.coordinator_main), "Sign in successful.", Snackbar.LENGTH_LONG)
+                        .show();
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Snackbar.make(findViewById(R.id.coordinator_main), "Sign in cancelled.", Snackbar.LENGTH_SHORT)
+                            .show();
+                    //showSnackbar(R.string.sign_in_cancelled);
+                    return;
                 }
-                // ...
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Snackbar.make(findViewById(R.id.coordinator_main), "No internet connection.", Snackbar.LENGTH_SHORT)
+                            .show();
+                    //showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Snackbar.make(findViewById(R.id.coordinator_main), "Unknown error.", Snackbar.LENGTH_SHORT)
+                            .show();
+                    //showSnackbar(R.string.unknown_error);
+                    return;
+                }
             }
-        };
+
+            Snackbar.make(findViewById(R.id.coordinator_main), "Unknown sign in response.", Snackbar.LENGTH_SHORT)
+                    .show();
+        }
     }
 
     @Override
@@ -71,14 +131,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
     }
 }
