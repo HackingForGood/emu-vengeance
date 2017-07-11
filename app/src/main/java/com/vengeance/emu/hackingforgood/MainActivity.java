c59fa5e0473b6d3d1d98003db6d82257ee55851a
Will <willzhang100@gmail.com>
2017-07-11 18:27:59 -0400

    Fixed sign in process. Added sign out functionality. Removed HomeActivity.

--- app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
+++ app/src/main/java/com/vengeance/emu/hackingforgood/MainActivity.java
@@ -3,106 +3,115 @@ package com.vengeance.emu.hackingforgood;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.ErrorCodes;
-import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ResultCodes;
-import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
-import static android.R.id.message;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private static final int RC_SIGN_IN = 100;
+    private static final int RC_SIGN_IN = 1;
+    private static final String ANONYMOUS = "Anonymous";
+
+    private ListView mOffersListView;
+    private Button mGiveBtn;
+    private Button mTakeBtn;
+    private String mUsername;
 
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        // Initialize references to views
+        mOffersListView = (ListView) findViewById(R.id.offersListView);
+        mGiveBtn = (Button) findViewById(R.id.btn_give);
+        mTakeBtn = (Button) findViewById(R.id.btn_take);
+
+        mUsername = ANONYMOUS;
 
         mFirebaseAuth = FirebaseAuth.getInstance();
-        if(mFirebaseAuth.getCurrentUser() != null) {
-            // Already signed in
-            startActivity(new Intent(this, HomeActivity.class));
-        } else {
-            // Not signed in
-            Log.d(TAG, "Not signed in.");
-            startActivityForResult(
-                    // Get an instance of AuthUI based on the default app
-                    AuthUI.getInstance()
-                            .createSignInIntentBuilder()
-                            .setIsSmartLockEnabled(!BuildConfig.DEBUG)
-                            .setAvailableProviders(
-                                    Arrays.asList(
-                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
-                    .build(),
-                    RC_SIGN_IN
-            );
-        }
+
+        mGiveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Give food!", Snackbar.LENGTH_LONG).show();
+                startActivity(new Intent(getApplicationContext(), GiveActivity.class));
+            }
+        });
+
+        mTakeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Take food!", Snackbar.LENGTH_LONG).show();
+                startActivity(new Intent(getApplicationContext(), TakeActivity.class));
+            }
+        });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null) {
+                    // Already signed in
+                    onSignInInitialize();
+                    Toast.makeText(MainActivity.this, "Welcome to Snack Swap!", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Not signed in
+                    onSignOutCleanUp();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(!BuildConfig.DEBUG)
+                                    .setAvailableProviders(
+                                            Arrays.asList(
+                                                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN
+                    );
+                }
+            }
+        };
+    }
+
+    private void onSignInInitialize() {
+        // Attach database read listener
+    }
+
+    private void onSignOutCleanUp() {
+        // Detach database read listener
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
         if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
 
             // Successfully signed in
             if (resultCode == ResultCodes.OK) {
-                IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
-                String type = idpResponse.getProviderType();
-                Log.d(TAG, type);
-                Snackbar.make(findViewById(R.id.coordinator_main), "Sign in successful.", Snackbar.LENGTH_LONG)
-                        .show();
-                finish();
-                return;
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    Snackbar.make(findViewById(R.id.coordinator_main), "Sign in cancelled.", Snackbar.LENGTH_SHORT)
-                            .show();
-                    //showSnackbar(R.string.sign_in_cancelled);
-                    return;
-                }
 
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    Snackbar.make(findViewById(R.id.coordinator_main), "No internet connection.", Snackbar.LENGTH_SHORT)
-                            .show();
-                    //showSnackbar(R.string.no_internet_connection);
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    Snackbar.make(findViewById(R.id.coordinator_main), "Unknown error.", Snackbar.LENGTH_SHORT)
-                            .show();
-                    //showSnackbar(R.string.unknown_error);
-                    return;
-                }
             }
-
-            Snackbar.make(findViewById(R.id.coordinator_main), "Unknown sign in response.", Snackbar.LENGTH_SHORT)
-                    .show();
         }
     }
 
@@ -123,6 +132,9 @@ public class MainActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        } else if (id == R.id.sign_out_menu) {
+            AuthUI.getInstance().signOut(this);
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -136,5 +148,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 }
